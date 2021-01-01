01407c5263a6b79d3b64704b36cb25ebf2d25a84
Nikita Tupikov <nikita.tupikov@developex.net>
2021-01-01 15:08:09 +0200

    Create GameModeBase class

---
+++ Source/ToonTanks/GameModes/TankGameModeBase.cpp
@@ -0,0 +1,19 @@
+#include "TankGameModeBase.h"
+
+ATankGameModeBase::ATankGameModeBase() {}
+
+void ATankGameModeBase::ActorDied(AActor* DeadActor) {
+
+}
+
+void ATankGameModeBase::BeginPlay() {
+
+}
+
+void ATankGameModeBase::HandleGameStart() {
+
+}
+
+void ATankGameModeBase::HandleGameOver(bool PlayerWon) {
+
+}
