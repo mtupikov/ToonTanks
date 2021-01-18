5140d9ac326aa53aba643e4a9d028c82c53d7d02
Nikita Tupikov <nikita.tupikov@developex.net>
2021-01-19 00:31:39 +0200

    Implement basic BulletProjectile
    Summary:
    - Create BulletProjctile class;
    - Also, fix a little other projectile code.
    Done: trello.com/c/jFqkCLHA

---
+++ Source/TowerOffence/Actors/BulletProjectile.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ProjectileBase.h"
+#include "BulletProjectile.generated.h"
+
+UCLASS()
+class TOWEROFFENCE_API ABulletProjectile : public AProjectileBase {
+	GENERATED_BODY()
+};
